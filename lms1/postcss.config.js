542128a58e3981004b236c986959411605460826
rafsan619 <unknownyo66@gmail.com>
2025-03-05 05:17:03 +0600

    PostCSS bug fixed

--- lms1/postcss.config.js
+++ lms1/postcss.config.js
@@ -1,5 +1,7 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+// Change from ES module syntax to CommonJS
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-
-export default config;
